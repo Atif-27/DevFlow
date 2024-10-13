d4a9a10a5421b045817d0407b9e6b5012a03e209
Atif Ali <atif276ali@gmail.com>
2024-10-14 01:26:48 +0530

    feat(auth): Add Clerk Authentication

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,8 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home () {
+export default function Home() {
   return (
-   <main>
-    Home Page
-   </main>
-  )
+    <main>
+      Home Page
+      <UserButton />
+    </main>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,7 +73,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [tailwindcssAnimate, typography],
-
+  plugins: [],
+  // plugins: [tailwindcssAnimate, typography],
 };
 export default config;
