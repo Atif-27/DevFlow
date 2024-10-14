fad078ec13683e3e0b75cfbc77519411b3bd83d7
Atif Ali <atif276ali@gmail.com>
2024-10-14 21:14:53 +0530

    add(sidebar): add root left & right sidebar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,6 +1,6 @@
-// import LeftSidebar from '@/components/shared/LeftSidebar'
-// import RightSidebar from '@/components/shared/RightSidebar'
+import LeftSidebar from "@/components/shared/LeftSidebar";
 import Navbar from "@/components/shared/navbar/Navbar";
+import RightSidebar from "@/components/shared/RightSidebar";
 // import { Toaster } from '@/components/ui/toaster'
 import React from "react";
 
@@ -9,12 +9,11 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <main className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
-        {/* <LeftSidebar /> */}
-        Left Sidebar
+        <LeftSidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        {/* <RightSidebar /> */}
+        <RightSidebar />
         right sidebar
       </div>
 
